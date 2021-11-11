b0f15e03d552e9d429cdde5c026c336c9f53c246
Ali <forsteve79@gmail.com>
2021-11-11 19:11:45 +0200

    Finished the rest of Routes With testing its

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -31,16 +31,40 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const userInput = req.body.tag_name;
+    if (!userInput || userInput === "") return res.status(400).json('No or Invalid Tag Name');
+    const newTag = await Tag.create({ tag_name: userInput });
+    res.json(newTag);
+  } catch (error) {
+    res.status(500).json(`Error Ocurred: ${error.message}`);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const userInput = req.body.tag_name;
+    if (!userInput || userInput === "") return res.status(400).json('No or Invalid Tag Name');
+    const updatedTag = await Tag.update({ tag_name: userInput }, { where: { id: req.params.id } });
+    if (!updatedTag[0]) return res.status(404).json("Tag was not found or Same Data Provided");
+    res.json(updatedTag);
+  } catch (error) {
+    res.status(500).json(`Error Ocurred: ${error.message}`);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagToDelete = await Tag.destroy({ where: { id: req.params.id } });
+    if (!tagToDelete) return res.status(404).json("Tag Was not Found");
+    res.json(tagToDelete);
+  } catch (error) {
+    res.status(500).json(`Error Ocurred: ${error.message}`);
+  }
 });
 
 module.exports = router;
