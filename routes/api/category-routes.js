bee56aece75c103f2c117eeb8ced0a76fd80f828
Ali <forsteve79@gmail.com>
2021-11-11 18:18:45 +0200

    Fullfilled The GIF 3 For Category's rest Routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -31,16 +31,46 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const category_input = req.body.category_name;
+    if (!category_input || category_input === "") return res.status(400).json("Invalid Category Name");
+    const newCategory = await Category.create({ category_name: category_input });
+    res.json(newCategory);
+  } catch (error) {
+    res.status(500).json(`Error Ocurred: ${error.message}`);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const category_input = req.body.category_name;
+    if (!category_input || category_input === "") return res.status(400).json("Invalid Category Name");
+    const updatedCategory = await Category.update({ category_name: category_input }, {
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!updatedCategory[0]) return res.status(404).json('Category Was not Found or Same Category Name');
+    res.json(updatedCategory);
+  } catch (error) {
+    res.status(500).json(`Error Ocurred: ${error.message}`);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryToDelete = await Category.destroy({
+      where: { id: req.params.id }
+    });
+    if (!categoryToDelete) return res.json('Category Was not Found');
+    res.json(categoryToDelete);
+  } catch (error) {
+    res.status(500).json(`Error Ocurred: ${error.message}`);
+  }
 });
 
 module.exports = router;
